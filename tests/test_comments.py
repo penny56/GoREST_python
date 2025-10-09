921c932411f3a800de437e7c26149f3562a61414
penny56 <penny56@163.com>
2025-10-09 23:36:09 +0800

    comments

--- tests/test_comments.py
+++ tests/test_comments.py
@@ -5,14 +5,62 @@ import random, json
 def test_create_comment():
 
     # POST /posts/{id}/comments
-    pass
+    with open(config.consts.POST_FILE_PATH, "r", encoding="utf-8") as f:
+        post_json = json.loads(f.read())
+
+    post_data = {
+        "post_id": post_json['id'],
+        "name": "Matt",
+        "email": "Matt@noreply.com",
+        "body": "This is comment content."
+    }
+
+    res = api.client.send_request(method="post",
+                                  uri="/public/v2"+"/"+"posts"+"/"+str(post_data["post_id"])+"/"+"comments",
+                                  headers=config.consts.HEADERS,
+                                  body=post_data,
+                                  expected_status=201)
+
+    print("\ntest_create_comment passed, comment id is:", str(json.loads(res.text)['id']))
+
+    # get current dir
+    with open(config.consts.COMMENT_FILE_PATH, "w", encoding="utf-8") as f:
+        f.write(res.text)
 
 def test_create_comment_failure():
 
     # miss email or body field
-    pass
+    with open(config.consts.POST_FILE_PATH, "r", encoding="utf-8") as f:
+        data_json = json.loads(f.read())
+
+    data = {
+        "post_id": data_json['id'],
+        "name": "Matt",
+        "body": "This is comment content."
+    }
+
+    res = api.client.send_request(method="post",
+                                  uri="/public/v2"+"/"+"posts"+"/"+str(data["post_id"])+"/"+"comments",
+                                  headers=config.consts.HEADERS,
+                                  body=data,
+                                  expected_status=422)
+
+    print("test_create_comment_failure passed")
 
 def test_list_comments():
 
     # GET /posts/{id}/comments
-    pass
+    with open(config.consts.POST_FILE_PATH, "r", encoding="utf-8") as f:
+        post_json = json.loads(f.read())
+
+    res = api.client.send_request(method="get",
+                                  uri="/public/v2"+"/"+"posts"+"/"+str(post_json['id'])+"/"+"comments",
+                                  headers=config.consts.HEADERS,
+                                  expected_status=200)
+    comments = json.loads(res.text)
+
+    for comment in comments:
+        print(f"comment id: {comment['id']}, post id: {comment['post_id']}, body: {comment['body']}")
+
+    print("test_list_comments passed")
+    
